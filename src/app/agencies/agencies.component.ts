6d0ebbd191eca332658ca2dfd9016876474fd14d
mgmg <medoelsayad229@gmail.com>
2022-05-21 23:25:26 +0200

    agencies

--- src/app/agencies/agencies.component.ts
+++ src/app/agencies/agencies.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ECats } from '../app-constants';
+import { FirebaseService } from '../firebase.service';
+import { Car } from '../shared/models/car.model';
 
 @Component({
   selector: 'app-agencies',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgenciesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  agencies: Car[] = [];
+  constructor(private spinner: NgxSpinnerService,
+    private fbService: FirebaseService,
+    ) { }
+  
+  ngOnInit():void
+   {
+     this.spinner.show();
+    
+     this.fbService.getData(ECats.CarService).subscribe(res => {
+       console.log(res);
+       this.agencies = res;
+       this.spinner.hide();
+     })
   }
-
 }
