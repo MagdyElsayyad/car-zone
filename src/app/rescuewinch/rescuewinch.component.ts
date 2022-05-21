8a4134661b38b526199437177153df91ba6ab82a
mgmg <medoelsayad229@gmail.com>
2022-05-21 22:07:28 +0200

    winches

--- src/app/rescuewinch/rescuewinch.component.ts
+++ src/app/rescuewinch/rescuewinch.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ECats } from '../app-constants';
+import { FirebaseService } from '../firebase.service';
+import { Car } from '../shared/models/car.model';
 
 @Component({
   selector: 'app-rescuewinch',
@@ -7,12 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RescuewinchComponent implements OnInit {
 
-  constructor() { }
-
+  rescuewinches: Car[] = [];
+  constructor(private spinner: NgxSpinnerService,private fbService: FirebaseService) { }
+  
   ngOnInit():void
    {
-  
+     this.spinner.show();
 
+     this.fbService.getData(ECats.RescueWinch).subscribe(res => {
+       this.rescuewinches = res;
+       console.log(res);
+       this.spinner.hide();
+     })
   }
 
 }
